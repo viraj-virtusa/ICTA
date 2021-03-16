5851af23d58d37867401344b8d3af95a53583b6e
Viraj Kulathunga <vccran@gmail.com>
2021-03-16 14:54:24 +0530

    test

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World Viraj";
+		return "Hello Docker World Viraj a";
 	}
 
 	public static void main(String[] args) {
